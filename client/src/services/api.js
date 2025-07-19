24371acb2c474f80ef3aa424650b2893692ecbac
allan-bit-web <allankinuthia59@gmail.com>
2025-07-19 09:28:58 +0300

    changed the local backend url to deployed live url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Create axios instance with base URL
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://blog-application-backend-ix04.onrender.com/api'
   // ,headers: {
   //   'Content-Type': 'application/json',
   // },
